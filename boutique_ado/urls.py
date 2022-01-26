0c2d623e5f060f4e6f79c101c2cb9eb465a4df43
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-01-26 19:25:37 +0100

    complete homepage header and css

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
+from os import stat
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +27,4 @@ urlpatterns = [
     path('', include('home.urls')),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
