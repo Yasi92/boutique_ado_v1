da9b1ec3eed0ac5a9f453fa6f1ff0fa977d6d18d
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-01-28 17:53:57 +0100

    Add sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -19,14 +19,24 @@ def all_products(request):
     if request.GET:
 
         if 'sort' in request.GET:
-            sortkey = request.GET['sort']
+            sortkey = request.GET['sort'] # Create the sortkey variable
+            # Make a copy of it and call it sort, which we'll use later to construct current_sorting = f'{sort}_{direction}'
             sort = sortkey
-            if sortkey == 'name':
+            if sortkey == 'name': # If the field we want to sort on is 'name'...
+                 # Let's actually sort (i.e. order_by) on one called 'lower_name', in order to ensure it doesn't order Z before a just because the Z is uppercase
                 sortkey = 'lower_name'
+                # Annotate all the products w/ a new field, lower_name=Lower('name') and sort based on it
                 products = products.annotate(lower_name = Lower('name'))
+            
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
+
+                # by default the sorting is asc
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
 
@@ -50,12 +60,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains= query)
             products = products.filter(queries)
 
+    # Now we still have sort = 'name' to construct current_sorting later
+    # but we've sorted on a different field called lower_name
     current_sorting = f'{sort}_{direction}'
 
     context = {
         'products' : products,
         'search_term' : query,
-        'current_category' : categories,
+        'current_categories' : categories,
         'current_sorting' :current_sorting,
     }
 
