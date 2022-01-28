20e26bff4037819bd7330befa4f8d230465edae4
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-01-28 12:57:58 +0100

    add product filter functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from audioop import reverse
+from email import message
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.db.models import Q
 from .models import Product
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,9 +11,22 @@ from .models import Product
 def all_products(request):
     '''A view to show all products, including sorting and search queries '''
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            # if the field is blank
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains= query)
+            products = products.filter(queries)
 
     context = {
         'products' : products,
+        'search_term' : query,
     }
 
     return render(request, 'products/products.html', context)
