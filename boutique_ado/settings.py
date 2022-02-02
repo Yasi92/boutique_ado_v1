c095ab241118c460f3ea476f913462c082d49efb
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-02-02 14:04:25 +0100

    add basic checkout

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv 
+load_dotenv() 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -177,7 +180,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+#  stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 
--- checkout/views.py
+++ checkout/views.py
@@ -2,23 +2,46 @@ from audioop import reverse
 from email import message
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.conf import settings
+
+import stripe
 
 
 from checkout.forms import OrderForm
+from bag.contexts import bag_contents
 
 # Create your views here.
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount= stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+                                Did you forget to set it in your enviroment?')
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form' : OrderForm,
+        'stripe_public_key' : stripe_public_key,
+        'client_secret' : intent.client_secret,
 
     }    
     return render(request, template, context)
