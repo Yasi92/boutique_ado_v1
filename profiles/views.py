aa73d0ddfbe7274711b29ced40e8b1c3dfa44f42
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-02-10 11:38:54 +0100

    Secure the add, edit, delete and profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 from .models import UserProfile
@@ -7,7 +8,7 @@ from .forms import UserProfileForm
 
 from checkout.models import Order
 
-
+@login_required
 def profile(request):
     """ Display the user's profile. """
 
@@ -34,7 +35,6 @@ def profile(request):
     }
 
     return render(request, template, context)
-
 
 
 def order_history(request, order_number):
